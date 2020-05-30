3b6f6b236735e2eef6eb1aedc2c05acab92859d2
Weslley Almeida <weslley@weslley.almd>
2020-05-30 12:44:34 -0300

    Fix transfer destination_account_id and amount data type

--- db/migrate/20200530153501_create_transfers.rb
+++ db/migrate/20200530153501_create_transfers.rb
@@ -2,8 +2,8 @@ class CreateTransfers < ActiveRecord::Migration[6.0]
   def change
     create_table :transfers do |t|
       t.references :account, null: false, foreign_key: true
-      t.string :destination_account_id
-      t.string :amount
+      t.integer :destination_account_id
+      t.integer :amount
 
       t.timestamps
     end
