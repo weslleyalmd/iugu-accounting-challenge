9cfe4407b8a259ca2b8fa3977f1083b148564ef2
Weslley Almeida <weslley@weslley.almd>
2020-06-01 15:16:43 -0300

    Change account serializer to return balance in Reais

--- app/serializers/complete_account_serializer.rb
+++ app/serializers/complete_account_serializer.rb
@@ -1,3 +1,9 @@
 class CompleteAccountSerializer < ActiveModel::Serializer
+  include ActionView::Helpers::NumberHelper
+
   attributes :id, :name, :balance
+
+  def balance
+    number_to_currency(object.balance / 100, :unit => "R$ ", :separator => ",", :delimiter => ".")
+  end
 end
